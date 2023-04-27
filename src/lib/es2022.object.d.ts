70cb8c3805851aa9eaf50902ccf53d3238f9ed3c
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2023-04-27 12:44:33 +0900

    Update es2022.object.d.ts

--- src/lib/es2022.object.d.ts
+++ src/lib/es2022.object.d.ts
@@ -5,4 +5,11 @@ interface ObjectConstructor {
      * @param v A property name.
      */
     hasOwn<T extends {}, K extends string>(o: T, v: K): o is T & { [key in K]: unknown};
+    
+    /**
+     * Determines whether an object has a property with the specified name.
+     * @param o An object.
+     * @param v A property name.
+     */
+    hasOwn(o: object, v: PropertyKey): boolean;
 }
