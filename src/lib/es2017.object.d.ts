11ab798834a5bbbf71dc59630d0bb5afd11c2597
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2023-04-27 12:40:01 +0900

    Update entries type

--- src/lib/es2017.object.d.ts
+++ src/lib/es2017.object.d.ts
@@ -15,6 +15,12 @@ interface ObjectConstructor {
      * Returns an array of key/values of the enumerable properties of an object
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
+    entries<T extends {}>(o: T }): [keyof T, T[keyofT]][]
+
+    /**
+     * Returns an array of key/values of the enumerable properties of an object
+     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+     */
     entries<T>(o: { [s: string]: T } | ArrayLike<T>): [string, T][];
 
     /**
